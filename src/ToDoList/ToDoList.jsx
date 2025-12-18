88fa344b3359397e7be53108224865ae5e6a2476
Afraz Mishak <afrazmishak@gmail.com>
2025-12-18 17:00:46 +0400

    localStorage is added for ToDoList

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { TextInput } from "./components/TextInput"
 import { DisplayText } from "./components/DisplayText"
 
+const STORAGE_KEY = 'todo_tasks'
+
 export function ToDoList() {
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState(() => {
+        const storedTasks = localStorage.getItem(STORAGE_KEY)
+        return storedTasks ? JSON.parse(storedTasks) : []
+    });
+
+    useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks))
+    }, [tasks]);
     
     return (
         <>
