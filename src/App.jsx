d3af2b1a4efef0d42231a725ccbe47db2cc387b5
Afraz Mishak <afrazmishak@gmail.com>
2025-12-26 13:57:18 +0400

    Home page needs to be hidden since under development

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import {Routes, Route } from 'react-router-dom'
+import {Routes, Route, Navigate } from 'react-router-dom'
 import ToDoList from './ToDoList/ToDoList'
 import Home from './ToDoList/components/Home'
 import './App.css'
 
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigate to="/todo-list" />} />
+      
+      <Route path="/home" element={<Home />} />
       <Route path="/todo-list" element={<ToDoList />} />
     </Routes>  
   )
