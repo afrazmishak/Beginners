fbe1aef8d99ce731f7a1f506f5dd85b92992f819
Afraz Mishak <afrazmishak@gmail.com>
2026-01-02 22:01:34 +0400

    Certain changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import {Routes, Route, Navigate } from 'react-router-dom'
 import ToDoList from './ToDoList/ToDoList'
-import Home from './ToDoList/components/Home'
+import Home from './Home/Home'
 import './App.css'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/todo-list" />} />
+      <Route path="/" element={<Navigate to="/todo-list" replace />} />
       
       <Route path="/home" element={<Home />} />
       <Route path="/todo-list" element={<ToDoList />} />
-    </Routes>  
+    </Routes>
   )
 }
 
